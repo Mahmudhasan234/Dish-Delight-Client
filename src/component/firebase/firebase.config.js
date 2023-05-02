992d12283a4b7cb3b389be65a43c95651adea5eb
unknown <sumaiya.rrashid@gmail.comm>
2023-05-03 01:33:15 +0200

    env.local done

--- src/component/firebase/firebase.config.js
+++ src/component/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITTE_APIKEY,
-  authDomain: import.meta.env.VITTE_AUTHDOMAIN,
-  projectId: import.meta.env.VITTE_PROJECTID,
-  storageBucket: import.meta.env.VITTE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITTE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITTE_APPID
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
