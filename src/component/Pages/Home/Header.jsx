72d7605f98e0c733815355ca2d33d3812478393f
unknown <sumaiya.rrashid@gmail.comm>
2023-05-03 00:29:32 +0200

    login page done

--- src/component/Pages/Home/Header.jsx
+++ src/component/Pages/Home/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
                 {/* heading div */}
                 <div className=''><h1 className='lg:text-7xl mb-5 lg:mb-10 text-5xl font-bold'>Make Your food special with our <br />
                 <span>Recipes</span></h1>
-                 <button className='text-white px-5 py-3 rounded-full bg-gradient-to-r from-[#f12711] to-[#f5af19]'> <Link> Browse Recipes</Link> </button>
+                 <button className='text-white font-bold px-5 py-3 rounded-full bg-gradient-to-r from-yellow-400 to-red-400'> <Link> Browse Recipes</Link> </button>
                  </div>
                 {/* heading picture div  */}
                 <div className=' w-full'><img src='https://i.ibb.co/6RtTg0G/chef-cooking.jpg' alt="" /></div>
